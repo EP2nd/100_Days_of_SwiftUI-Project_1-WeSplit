e289550608537fa707bf7e65dac9a1dab0b83e18
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-11-11 15:16:44 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Edwin Przeźwiecki Jr. on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
